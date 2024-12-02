366e9c09c66dbae93e12a1274f726d89a077647a
Lili <lili.berenyi@gmail.com>
2024-12-02 10:21:08 +0000

    Add empty project

---
+++ cs-asynchronous-csharp-sprint/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cs_asynchronous_csharp_sprint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
